cd0e9527b51bf80cbb718e68fa248105c6a7c1df
jumper385 <jumper385@email.com>
2026-06-14 00:12:52 +0800

    fix: main int output

--- app/src/main.c
+++ app/src/main.c
@@ -3,9 +3,10 @@
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(main);
 
-void main(void) {
+int main(void) {
   while (1) {
     LOG_INF("Hello, World from Zephyr!");
     k_msleep(1000);
   }
+  return 1;
 }
